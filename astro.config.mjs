12c29340c1664ac4d8f4bf059fd7120513dfb4ca
kupeace <kuvempupmeb202@gmail.com>
2026-09-09 15:51:50 +0530

    Update site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,7 +38,7 @@ const viteConfig = {
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
-  site: 'https://accessible-astro-starter.incluud.dev',
+  site: 'https://kupeace.github.io',
   integrations: [compress(), icon(), mdx(), sitemap()],
   vite: viteConfig,
   env: {
